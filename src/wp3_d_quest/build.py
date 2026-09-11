1fd95d56bc646f7741a3ca903a77e64f57696704
Marton Vago <marci.vago@gmail.com>
2026-09-11 10:46:05 +0100

    refactor: ♻️ download metadata to raw and staging

--- src/wp3_d_quest/build.py
+++ src/wp3_d_quest/build.py
@@ -9,29 +9,32 @@ from wp3_d_quest import common, metadata
 common.dotenv.load_env_vars()
 
 SRC = Path(str(files("wp3_d_quest"))).joinpath("..").resolve()
-BLD = SRC.joinpath("..", "bld").resolve()
+RAW = SRC.joinpath("..", "raw").resolve()
+STAGING = SRC.joinpath("..", "staging").resolve()
 
-BLD_REDCAP = BLD / "redcap"
-METADATA_PATH = BLD_REDCAP / "metadata.json"
+RAW_METADATA_PATH = RAW / "metadata" / "metadata.json"
+STAGING_METADATA_PATH = STAGING / "metadata" / "metadata.json"
 
 DATAPACKAGE_PATH = SRC.parent / "datapackage.json"
 
 
 @mark.metadata
 def task_download_metadata(
-    metadata_path: Annotated[Path, Product] = METADATA_PATH,
+    raw_metadata_path: Annotated[Path, Product] = RAW_METADATA_PATH,
+    staging_metadata_path: Annotated[Path, Product] = STAGING_METADATA_PATH,
 ) -> None:
-    """Download the metadata to `BLD_REDCAP`."""
+    """Download the metadata to raw and staging."""
     redcap_metadata = common.redcap.get_json("metadata")
-    common.json.write(metadata_path, redcap_metadata)
+    common.json.write(raw_metadata_path, redcap_metadata)
+    common.json.write(staging_metadata_path, redcap_metadata)
 
 
 @mark.metadata
 def task_create_datapackage_json(
     datapackage_path: Annotated[Path, Product] = DATAPACKAGE_PATH,
-    metadata_path: Path = METADATA_PATH,
+    staging_metadata_path: Path = STAGING_METADATA_PATH,
 ) -> None:
     """Create the datapackage.json file from the REDCap metadata."""
-    redcap_metadata = common.json.read(metadata_path)
-    datapackage = metadata.redcap.create_datapackage(redcap_metadata)
+    staging_metadata = common.json.read(staging_metadata_path)
+    datapackage = metadata.redcap.create_datapackage(staging_metadata)
     common.json.write(datapackage_path, datapackage)
